fd125ffbbb3bbf49cf06766dd0f1929cb028dcc1
m4md24 <m4md24@gmail.com>
2025-07-01 04:34:52 +0300

    Initialize (Calculator) Project

---
+++ src/_2_projects/_2_1_calculator/App.cs
@@ -0,0 +1,9 @@
+namespace OOP_as_it_should_be_in_CS_by_Programming_Advices._2_projects._2_1_calculator;
+
+public class App {
+    public static void Main(
+        string[] args
+    ) {
+
+    }
+}
