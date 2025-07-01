3bd7a06b2d986594ff6e78aa06e959c39162d73c
m4md24 <m4md24@gmail.com>
2025-07-01 04:38:02 +0300

    Create Operators

--- src/_2_projects/_2_1_calculator/Calculator.cs
+++ src/_2_projects/_2_1_calculator/Calculator.cs
@@ -1,5 +1,11 @@
 namespace OOP_as_it_should_be_in_CS_by_Programming_Advices._2_projects._2_1_calculator;
 
 public class Calculator {
-    
+    private enum Operator {
+        Add      = '+',
+        Subtract = '-',
+        Multiply = '*',
+        Divide   = '/',
+        Clear    = ' '
+    };
 }
