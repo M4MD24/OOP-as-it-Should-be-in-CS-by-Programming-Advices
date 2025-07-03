25ff0203339215322d3af50ff49467904d9bf2d8
m4md24 <m4md24@gmail.com>
2025-07-04 00:04:54 +0300

    Learn (Sealed Class) Concept

---
+++ src/_1_concepts/_1_13_sealed/_1_13_1_sealed_class/SealedClass.cs
@@ -0,0 +1,10 @@
+namespace OOP_as_it_should_be_in_CS_by_Programming_Advices._1_concepts._1_13_sealed._1_13_1_sealed_class;
+
+// Can't inherit from a sealed class
+sealed class FirstClass {}
+
+class Program {
+    static void Main(
+        string[] args
+    ) {}
+}
